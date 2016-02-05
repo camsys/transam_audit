abedc516b711d3c60c593da98f4cb3783ad853f4
Lydia Chang <mathmerized@gmail.com>
2016-02-05 13:19:24 -0500

    add permission to rspec viow

--- spec/views/audit_results/_index_actions.html.haml_spec.rb
+++ spec/views/audit_results/_index_actions.html.haml_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 describe "audit_results/_index_actions.html.haml", :type => :view do
   it 'links' do
+    allow(controller).to receive(:current_ability).and_return(Ability.new(create(:admin)))
     render
 
     FileContentType.all.each do |file_type|
