9068ef69bc24dd9ac2e2778fd5880d087644e1eb
Lydia Chang <mathmerized@gmail.com>
2016-11-22 07:48:31 -0500

    fix syntax error

--- app/models/abilities/admin_audit_ability.rb
+++ app/models/abilities/admin_audit_ability.rb
@@ -7,7 +7,7 @@ module Abilities
       cannot :create, Audit   # temporarily disallow creation since creating customized audit not implemented y
 
       cannot :destroy, Activity do |a|
-      (a.system_activity == true) || Audit.pluck(:activity_id).include? a.id
+        (a.system_activity == true) || (Audit.pluck(:activity_id).include? a.id)
       end
 
     end
