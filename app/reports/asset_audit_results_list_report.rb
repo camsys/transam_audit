d0cf3e80e40300420440cb3d6559532109eb9918
Lydia Chang <mathmerized@gmail.com>
2017-01-12 09:04:01 -0500

    [fixes#137350997] [#135339191] explicitly set class to Asset for getting audit results so properly search audit results with Asset criteria

--- app/reports/asset_audit_results_list_report.rb
+++ app/reports/asset_audit_results_list_report.rb
@@ -1,5 +1,5 @@
 class AssetAuditResultsListReport < Report
   def get_data(conditions)
-    AuditResult.search_auditable(conditions, @auditable_type, {disposition_date: nil})
+    AuditResult.search_auditable(conditions, 'Asset', {disposition_date: nil})
   end
 end
