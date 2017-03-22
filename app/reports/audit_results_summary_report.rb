bed867440a8a1d7f6eccb0064ba6fb25090d7b19
Xudong Liu <xudong.camsys@gmail.com>
2017-03-22 14:01:21 -0400

    Performance tweaks

--- app/reports/audit_results_summary_report.rb
+++ app/reports/audit_results_summary_report.rb
@@ -10,24 +10,26 @@ class AuditResultsSummaryReport < AbstractReport
     Rails.logger.debug "Org Ids = #{organization_id_list}"
 
     # Process each org in turn
+    org_data = Organization.where(id: organization_id_list).pluck(:id, :short_name).to_h
     organization_id_list.each do |org_id|
-
-      org = Organization.find(org_id)
-      Rails.logger.debug "Summarizing for #{org.short_name}"
+      org_short_name = org_data[org_id]
+      Rails.logger.debug "Summarizing for #{org_short_name}"
 
       # Summarize this org/audit combination by class name. This return an array
       # of class names and counts
       audit_results_criteria = {:audit_id => audit.id, :organization_id => org_id, :auditable_type => type}
       results = AuditResult.search_auditable(audit_results_criteria, type, audited_must_meet, audited_must_not_meet)
       summary = results.group(:class_name).count
+      failed_summary = results.where(:audit_result_type => AuditResultType::AUDIT_RESULT_FAILED).group(:class_name).count
       summary.each do |class_name, count|
         if count > 0
-          total   = results.where(:class_name => class_name).count
-          failed  = results.where(:class_name => class_name, :audit_result_type => AuditResultType::AUDIT_RESULT_FAILED).count
-          data << [org.short_name, class_name, total, (total - failed), failed]
+          total   = count
+          failed  = failed_summary[class_name].to_i
+          data << [org_short_name, class_name, total, (total - failed), failed]
         end
       end
     end
+
     return [labels, data]
   end
 
