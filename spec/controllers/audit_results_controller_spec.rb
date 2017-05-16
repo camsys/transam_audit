84c5d5ba8fa7e44be2ef1c84d63026549e65fe8e
Lydia Chang <mathmerized@gmail.com>
2017-05-16 12:04:32 -0400

    update tests to reflect change to use org filter in audit_results#index

--- spec/controllers/audit_results_controller_spec.rb
+++ spec/controllers/audit_results_controller_spec.rb
@@ -14,9 +14,8 @@ RSpec.describe AuditResultsController, :type => :controller do
     test_result = create(:audit_result, :auditable => test_asset, :audit_result_type_id => 2, :organization => test_asset.organization)
 
 
-    get :index, :org_filter => subject.current_user.organization.id, :types_filter => ['Asset'], :audit_filter => test_result.audit.id
+    get :index, :types_filter => ['Asset'], :audit_filter => test_result.audit.id
 
-    expect(assigns(:org_filter)).to eq(subject.current_user.organization.id.to_s)
     expect(assigns(:types_filter)).to eq(['Asset'])
     expect(assigns(:audit_filter)).to eq(test_result.audit.id.to_s)
     expect(assigns(:audit_result_type_filter)).to eq(2)
