385778abc803d4e055beb25f4db3172974baa6b9
mathmerized <mathmerized@gmail.com>
2018-01-24 10:04:25 -0500

    update test - frequenecy of cron job only set thru admin interface

--- spec/views/audits/_form.html.haml_spec.rb
+++ spec/views/audits/_form.html.haml_spec.rb
@@ -9,7 +9,6 @@ describe "audits/_form.html.haml", :type => :view do
     #expect(rendered).to have_field('audit_auditor_class_name') # disabled class since no interactive audit creating
     expect(rendered).to have_field('audit_activity_attributes_start_date')
     expect(rendered).to have_field('audit_activity_attributes_end_date')
-    expect(rendered).to have_field('audit_activity_attributes_frequency_type_id')
     expect(rendered).to have_field('audit_activity_attributes_show_in_dashboard')
     expect(rendered).to have_field('audit_activity_attributes_active')
     expect(rendered).to have_field('audit_start_date')
