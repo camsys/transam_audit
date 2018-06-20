c4a8fd259d42a44c081f48cced8dade15bbcc5a7
mathmerized <mathmerized@gmail.com>
2018-06-20 12:11:01 -0400

    cleanup system config extensions, mixin links to transam assets

--- db/data_migrations/20180606131241_add_audit_system_config_extensions.rb
+++ db/data_migrations/20180606131241_add_audit_system_config_extensions.rb
@@ -1,20 +1,7 @@
 class AddAuditSystemConfigExtensions < ActiveRecord::DataMigration
   def up
-    # currently add auditable mixin to old assets as well
     system_config_extensions = [
-        {class_name: 'Equipment', extension_name: 'TransamAuditable', active: true},
-        {class_name: 'Vehicle', extension_name: 'TransamAuditable', active: true},
-        {class_name: 'Locomotive', extension_name: 'TransamAuditable', active: true},
-        {class_name: 'RailCar', extension_name: 'TransamAuditable', active: true},
-        {class_name: 'SupportVehicle', extension_name: 'TransamAuditable', active: true},
-        {class_name: 'TransitFacility', extension_name: 'TransamAuditable', active: true},
-        {class_name: 'SupportFacility', extension_name: 'TransamAuditable', active: true},
-
-        {class_name: 'CapitalEquipment', extension_name: 'TransamAuditable', active: true},
-        {class_name: 'FacilityComponent', extension_name: 'TransamAuditable', active: true},
-        {class_name: 'ServiceVehicle', extension_name: 'TransamAuditable', active: true},
-        {class_name: 'RevenueVehicle', extension_name: 'TransamAuditable', active: true},
-        {class_name: 'Facility', extension_name: 'TransamAuditable', active: true},
+        {class_name: 'TransamAsset', extension_name: 'TransamAuditable', active: true}
     ]
 
     system_config_extensions.each do |extension|
