71fa41fb2f1d40ca54d209c3f5e48fb855320afc
mathmerized <mathmerized@gmail.com>
2018-07-16 10:22:26 -0400

    fix system config extensions in db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,19 +30,7 @@ audits = [
 
 # currently add auditable mixin to old assets as well
 system_config_extensions = [
-    {class_name: 'Equipment', extension_name: 'TransamAuditable', active: true},
-    {class_name: 'Vehicle', extension_name: 'TransamAuditable', active: true},
-    {class_name: 'Locomotive', extension_name: 'TransamAuditable', active: true},
-    {class_name: 'RailCar', extension_name: 'TransamAuditable', active: true},
-    {class_name: 'SupportVehicle', extension_name: 'TransamAuditable', active: true},
-    {class_name: 'TransitFacility', extension_name: 'TransamAuditable', active: true},
-    {class_name: 'SupportFacility', extension_name: 'TransamAuditable', active: true},
-
-    {class_name: 'CapitalEquipment', extension_name: 'TransamAuditable', active: true},
-    {class_name: 'FacilityComponent', extension_name: 'TransamAuditable', active: true},
-    {class_name: 'ServiceVehicle', extension_name: 'TransamAuditable', active: true},
-    {class_name: 'RevenueVehicle', extension_name: 'TransamAuditable', active: true},
-    {class_name: 'Facility', extension_name: 'TransamAuditable', active: true},
+    {class_name: 'TransamAsset', extension_name: 'TransamAuditable', active: true}
 ]
 
 lookup_tables = %w{ audit_result_types }
