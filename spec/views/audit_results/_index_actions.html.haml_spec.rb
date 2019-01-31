bf074a2d8c15b2b32f7f753db82fab94524e1ef1
mathmerized <mathmerized@gmail.com>
2019-01-31 12:34:01 -0500

    tests

--- spec/views/audit_results/_index_actions.html.haml_spec.rb
+++ spec/views/audit_results/_index_actions.html.haml_spec.rb
@@ -5,7 +5,7 @@ describe "audit_results/_index_actions.html.haml", :type => :view do
     allow(controller).to receive(:current_ability).and_return(Ability.new(create(:admin)))
     render
 
-    FileContentType.all.each do |file_type|
+    FileContentType.where.not(name: 'New Inventory').each do |file_type|
       expect(rendered).to have_button(file_type.name)
     end
   end
