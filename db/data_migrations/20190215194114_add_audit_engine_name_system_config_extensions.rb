472cabb25259a7b30705b130d63f3e78fe2b283b
mathmerized <mathmerized@gmail.com>
2019-02-19 13:51:56 -0500

    spelling error

--- db/data_migrations/20190215194114_add_audit_engine_name_system_config_extensions.rb
+++ db/data_migrations/20190215194114_add_audit_engine_name_system_config_extensions.rb
@@ -1,5 +1,5 @@
 class AddAuditEngineNameSystemConfigExtensions < ActiveRecord::DataMigration
   def up
-    SystemConfigExetnsion.find_by({class_name: 'TransamAsset', extension_name: 'TransamAuditable', active: true}).update!(engine_name: 'audit')
+    SystemConfigExtension.find_by({class_name: 'TransamAsset', extension_name: 'TransamAuditable', active: true}).update!(engine_name: 'audit')
   end
 end
